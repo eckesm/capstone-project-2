22bbba61c3c11b4380c4674af69ba33d74948cca
Matthew Eckes <eckesm@gmail.com>
2021-11-18 10:37:46 -0500

    Adding CORS options.

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+// app.use(cors());
+const corsOptions = {
+	origin               : 'https://mre-capstone2-frontend.herokuapp.com',
+	optionsSuccessStatus : 200
+};
+app.use(cors(corsOptions));
+app.options('*', cors());
+
 app.use(express.json());
 app.use(authenticateJWT);
 
