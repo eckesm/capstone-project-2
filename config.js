08cea29399c78fe5bf964a2fb409c32c7d3dec74
Matthew Eckes <eckesm@gmail.com>
2021-11-18 11:15:07 -0500

    Adding database to project.

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
-const DB_URI = process.env.NODE_ENV === 'test' ? 'postgresql:///capstone2_test' : 'postgresql:///capstone2';
+// const DB_URI = process.env.NODE_ENV === 'test' ? 'postgresql:///capstone2_test' : 'postgresql:///capstone2';
+const DB_URI = process.env.DATABASE_URL;
 
 const SECRET_KEY = process.env.SECRET_KEY || 'secret';
 
