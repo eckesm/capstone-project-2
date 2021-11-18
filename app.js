434cc3aa06bf847ac7f50d5336821bdd60d3156a
Matthew Eckes <eckesm@gmail.com>
2021-11-18 10:18:56 -0500

    Trying to fix CORS.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 
 const { NotFoundError } = require('./expressError');
 const { authenticateJWT } = require('./middleware/auth');
@@ -16,7 +16,13 @@ const invoicesRoutes = require('./routes/invoices');
 const expensesRoutes = require('./routes/expenses');
 
 const app = express();
-app.use(cors());
+// app.use(cors());
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+});
+
 app.use(express.json());
 app.use(authenticateJWT);
 
