49e5831736694fd783ba8c1b2f9a7354a2443481
Matthew Eckes <eckesm@gmail.com>
2021-12-06 15:29:46 -0500

    Trying to fix connection issue.

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 const { Client } = require('pg');
-const { DB_URI } = require('./config');
+// const { DB_URI } = require('./config');
 const { getDatabaseUri } = require('./config');
 
 const client = new Client({
-	connectionString : getDatabaseUri()
+	connectionString : getDatabaseUri(),
+	ssl              : { rejectUnauthorized: false }
 });
 
 client.connect();
